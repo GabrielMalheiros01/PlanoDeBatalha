f137ef5af32ab872c99884ccb425b60207333bf7
Gabriel Malheiros <113548821+GabrielMalheiros01@users.noreply.github.com>
2024-10-13 15:12:50 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 from app import app
 import os
 
-SQLALCHEMY_DATABASE_URI = 'postgresql://materias_bd_user:MmzGj7PCeTSNKLVMNvyRjIvPGMW9rwSQ@dpg-cs5vuf3tq21c73dnl8t0-a.oregon-postgres.render.com/materias_bd''
+SQLALCHEMY_DATABASE_URI = 'postgresql://materias_bd_user:MmzGj7PCeTSNKLVMNvyRjIvPGMW9rwSQ@dpg-cs5vuf3tq21c73dnl8t0-a.oregon-postgres.render.com/materias_bd'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 UPLOAD_FOLDER = 'uploads'
